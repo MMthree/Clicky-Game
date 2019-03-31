808ddddc47fef2ea79ac2e2333404d84c5edd379
Mario <mmisthree@gmail.com>
2019-03-31 15:12:58 -0700

    Updates

---
+++ precache-manifest.b1cf2f4821f751144f76d65ea6720928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "d2b0a8b96107db0e17dd",
+    "url": "/Clicky-Game/static/js/main.bb721bcd.chunk.js"
+  },
+  {
+    "revision": "2ba9950c6dfa7945ee6f",
+    "url": "/Clicky-Game/static/js/2.6fe4059b.chunk.js"
+  },
+  {
+    "revision": "d2b0a8b96107db0e17dd",
+    "url": "/Clicky-Game/static/css/main.18318175.chunk.css"
+  },
+  {
+    "revision": "2ba9950c6dfa7945ee6f",
+    "url": "/Clicky-Game/static/css/2.d9515329.chunk.css"
+  },
+  {
+    "revision": "2509a51329d6e508d6b25b16f08e0228",
+    "url": "/Clicky-Game/index.html"
+  }
+];
